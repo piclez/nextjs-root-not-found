b3dcad6656e385009788d9e4587959c807096a53
Peter Dirickson <petetwd@gmail.com>
2024-01-02 19:53:08 -0800

    reverse layout with body to first

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -9,9 +9,9 @@ const MarketingLayout = ({
   children: React.ReactNode;
 }) => {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <div>
+      {children}
+    </div>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <div>
-      {children}
-    </div>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   )
 }
